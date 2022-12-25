0082fe43cb9299dde928f3c636d3b797f887acb7
Oussamajar <o.jaridi@gmail.com>
2022-12-25 18:46:46 +0100

    SG_commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'smartgarden-19ad6',
+    appId: '1:886957151820:web:f6046eda2b2f2456ad46e9',
+    databaseURL: 'https://smartgarden-19ad6-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'smartgarden-19ad6.appspot.com',
+    apiKey: 'AIzaSyCDV-THZfDcmcDXywbTcZPM9Kc-48CQ80E',
+    authDomain: 'smartgarden-19ad6.firebaseapp.com',
+    messagingSenderId: '886957151820',
+  },
+production: false
+};
